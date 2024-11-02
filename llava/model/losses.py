a3b2928fb42810ffc0730c11e92942f47427886b
rajibrhasan <rajib.rhasan@gmail.com>
2024-11-02 16:59:54 +0600

    Tweaking loss function

--- llava/model/losses.py
+++ llava/model/losses.py
@@ -54,28 +54,34 @@ class DiffLoss(nn.Module):
 
     def forward(self, input1, input2):
 
-        print('Input1 shape: ', input1.shape)
-        print('Input2 shape : ', input2.shape)
-
-        batch_size, sq_len, _ = input1.shape
-        input1 = input1.view(batch_size * sq_len, -1)
-        input2 = input2.view(batch_size * sq_len, -1)
-
         # Zero mean
         input1_mean = torch.mean(input1, dim=0, keepdims=True)
         input2_mean = torch.mean(input2, dim=0, keepdims=True)
         input1 = input1 - input1_mean
         input2 = input2 - input2_mean
 
-        print('Input1 shape: ', input1.shape)
-        print('Input2 shape : ', input2.shape)
-
-        input1_l2_norm = torch.norm(input1, p=2, dim=1, keepdim=True).detach()
+        input1_l2_norm = torch.norm(input1, p=2, dim=-1, keepdim=True).detach()
         input1_l2 = input1.div(input1_l2_norm.expand_as(input1) + 1e-6)
         
 
-        input2_l2_norm = torch.norm(input2, p=2, dim=1, keepdim=True).detach()
+        input2_l2_norm = torch.norm(input2, p=2, dim=-1, keepdim=True).detach()
         input2_l2 = input2.div(input2_l2_norm.expand_as(input2) + 1e-6)
+
+        # if len(input1.shape) == 3:
+        #     batch_size, sq_len, _ = input1.shape
+        #     input1 = input1.view(batch_size * sq_len, -1)
+        # if len(input2.shape) == 3:
+        #     batch_size, sq_len, _ = input2.shape
+        #     input2 = input2.view(batch_size * sq_len, -1)
+
+        # assert input1.shape == input2.shape, 'Feature dimensions should be the same'
+
+        
+
+        print('Input1 shape: ', input1.shape)
+        print('Input2 shape : ', input2.shape)
+
+        
 
         diff_loss = (input1_l2.t().mm(input2_l2)).pow(2)
         diff_loss = torch.mean(diff_loss)
