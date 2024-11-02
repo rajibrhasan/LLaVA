e7867456d3e465b9e4826049e51084a0cc8d66a0
rajibrhasan <rajib.rhasan@gmail.com>
2024-11-02 17:29:18 +0600

    Almost final

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -28,12 +28,6 @@ from ..llava_arch import LlavaMetaModel, LlavaMetaForCausalLM
 from ..losses import *
 
 
-def orthogonal_loss_matrix(inputs1, inputs2):
-    dot_product = (inputs1 * inputs2).sum(dim=-1)
-    # Minimize the absolute value of the dot product
-    loss = dot_product.pow(2).mean()
-
-    return loss
 
 class LlavaConfig(LlamaConfig):
     model_type = "llava_llama"
@@ -118,7 +112,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         device = outputs['loss'].device
         if embeds is not None:
             diff_loss = self.loss_diff(embeds['img_embeds1'], embeds['img_embeds2']) 
-            # diff_loss += self.loss_diff(embeds['img_embeds2'], embeds['text_embeds'])
+            diff_loss += self.loss_diff(embeds['img_embeds2'].mean(dim = 1), embeds['text_embeds'])
             # sim_loss = self.loss_sim(embeds['img_embeds2'], embeds['text_embeds'], 5)
             outputs['loss'] += self.config.diff_loss_coef * diff_loss.to(device)
 
--- llava/model/losses.py
+++ llava/model/losses.py
@@ -53,6 +53,8 @@ class DiffLoss(nn.Module):
         super(DiffLoss, self).__init__()
 
     def forward(self, input1, input2):
+        print('Input1 shape: ', input1.shape)
+        print('input2 shape: ', input2.shape)
 
         # Zero mean
         input1_mean = torch.mean(input1, dim=0, keepdims=True)
@@ -63,26 +65,17 @@ class DiffLoss(nn.Module):
         input1_l2_norm = torch.norm(input1, p=2, dim=-1, keepdim=True).detach()
         input1_l2 = input1.div(input1_l2_norm.expand_as(input1) + 1e-6)
         
-
         input2_l2_norm = torch.norm(input2, p=2, dim=-1, keepdim=True).detach()
         input2_l2 = input2.div(input2_l2_norm.expand_as(input2) + 1e-6)
-
-
 
         if len(input1_l2.shape) == 3:
             batch_size, sq_len, _ = input1_l2.shape
             input1_l2 = input1_l2.view(batch_size * sq_len, -1)
-        if len(input2.shape) == 3:
+        if len(input2_l2.shape) == 3:
             batch_size, sq_len, _ = input2_l2.shape
             input2_l2 = input2_l2.view(batch_size * sq_len, -1)
 
-        # assert input1.shape == input2.shape, 'Feature dimensions should be the same'
-
-        
-
-        print('Input1 shape: ', input1.shape)
-        print('Input2 shape : ', input2.shape)
-
+        assert input1_l2.shape == input2_l2.shape, 'Input1_l2 and Input2_l2 should have the same shape'
         
 
         diff_loss = (input1_l2.t().mm(input2_l2)).pow(2)
