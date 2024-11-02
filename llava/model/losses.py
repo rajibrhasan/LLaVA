82ea8269fa218cd632a988c8aa09ee46ec8dd74f
Rakibul Hasan Rajib <rajib@ap21.uc.osg-htc.org>
2024-11-02 03:50:09 -0500

    Debug

--- llava/model/losses.py
+++ llava/model/losses.py
@@ -74,10 +74,10 @@ class DiffLoss(nn.Module):
         input2_l2_norm = torch.norm(input2, p=2, dim=1, keepdim=True).detach()
         input2_l2 = input2.div(input2_l2_norm.expand_as(input2) + 1e-6)
 
-        diff_loss = input1_l2.t().mm(input2_l2)
+        diff_loss = (input1_l2.t().mm(input2_l2)).pow(2)
         print('Diff Loss shape: ', diff_loss.shape)
-        
-        diff_loss = torch.mean(diff_loss.pow(2))
+
+        diff_loss = torch.mean(diff_loss)
 
         return diff_loss
 
